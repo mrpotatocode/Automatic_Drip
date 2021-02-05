cf5151b410178673f9502817bc3fdb7b44e70371
CODE, POTATO <t.rose.github@protonmail.com>
2021-02-05 11:52:01 -0500

    pilot script for testing
    minor update to 000 TLS

--- R/002_Scraping.R
+++ R/002_Scraping.R
@@ -1,9 +1,9 @@
 
 #### Preamble ####
-# Purpose: The purpose of this script is to download all of the coffees by The Library on EightOunceCoffee.ca
+# Purpose: The purpose of this script is to download all of the coffees by Pilot Coffee on EightOunceCoffee.ca
 # Author: Thomas Rosenthal
 # Email: t.rose.github@protonmail.com
-# Last updated: 4 Feb 2021
+# Last updated: 5 Feb 2021
 # Prerequisites: see description file, several packages
 # Misc: runs on github actions
 
@@ -25,11 +25,11 @@ library(here)
 rundate = toString(sapply(date(now()), gsub, pattern = "-", replacement = "", fixed = TRUE))
 
 #get and save data
-raw_data <- read_html("https://eightouncecoffee.ca/collections/the-library-specialty-coffee")
-write_html(raw_data,paste0("R/inputs/data/EightOunce/TLS_" ,rundate, ".html"))
+raw_data <- read_html("https://eightouncecoffee.ca/collections/pilot-coffee-roasters")
+write_html(raw_data,paste0("R/inputs/data/EightOunce/Pilot_" ,rundate, ".html"))
 
 #get roaster, its easier to have it seperate
-Roaster <- tibble("https://eightouncecoffee.ca/collections/the-library-specialty-coffee") %>% 
+Roaster <- tibble("https://eightouncecoffee.ca/collections/pilot-coffee-roasters") %>% 
   separate(1, into = c("1","Roaster"), sep = "collections/",  remove = TRUE) %>% 
   select(-1) %>% 
   sapply(gsub, pattern = "-", replacement = " ", fixed = TRUE) %>% 
@@ -60,7 +60,7 @@ coffee_names <- our_data %>%
 
 
 #covert to hypenated words (e.g. 'the-library-specialty-coffee-ethiopia-chelelektu-washed')
-#add prefix: 'https://eightouncecoffee.ca/collections/the-library-specialty-coffee/products/'
+#add prefix: 'https://eightouncecoffee.ca/collections/pilot-coffee-roasters/products/'
 hypenated_data <- coffee_names %>% select(lower_name) %>% 
   apply( MARGIN = 2, FUN = trimws) %>% 
   sapply(gsub, pattern = " ", replacement = "-", fixed = TRUE) %>% 
@@ -70,7 +70,7 @@ hypenated_data <- coffee_names %>% select(lower_name) %>%
   rename(Hypen_Name = 1) 
 
 #paste these to be URLs
-hypenated_data$URL <- paste0("https://eightouncecoffee.ca/collections/the-library-specialty-coffee/products/",hypenated_data[,1])
+hypenated_data$URL <- paste0("https://eightouncecoffee.ca/collections/pilot-coffee-roasters/products/",hypenated_data[,1])
 
 #build table
 hypenated_data <- cbind(lower_name = rownames(hypenated_data), hypenated_data) 
@@ -99,6 +99,7 @@ URLs <-  unlist(checkURLs)
 coffee_table <- data.frame()
 cnt <- 0
 
+
 for(i in URLs){
   coffee_row <- read_html(i)
   cnt <- cnt+1
@@ -113,7 +114,7 @@ for(i in URLs){
     html_text()
   
   #save the htmls
-  write_html(coffee_row,paste0("R/inputs/data/EightOunce/Coffees/TLS_", cnt, "_" ,rundate, ".html"))
+  write_html(coffee_row,paste0("R/inputs/data/EightOunce/Coffees/Pilot_", cnt, "_" ,rundate, ".html"))
   
   
   #tibble the character vector
@@ -121,11 +122,12 @@ for(i in URLs){
     tibble(raw_text = slightly_cleaned_coffee)
   
   #seperate details row into relevent columns
-  coffee <- coffee %>%
+  coffee <- coffee[2,] %>%
     pivot_wider(names_from = 1, values_from = raw_text) %>% 
-    rename(table = 1) %>%   #, detail1 = 2, detail2 = 3, detail3 = 4, detail4 = 5 -- some investigation into how to make this dynamic?
-    separate(table, into = c("1","Region","Variety","Processing","Altitude","TastingNotes"), sep = "\\w+:",  remove = TRUE) %>% 
-    select("Region","Variety","Processing","Altitude","TastingNotes") 
+    rename(table = 1) %>% 
+    separate(table, into = c("1","Availability","CoffeeType","Style","TastingNotes","DirectTrade","Producer",
+                             "Country","Region","Variety","Processing","Altitude"), sep = "\\b[A-Z][A-Z A-Z+:]*\\b",  remove = TRUE) %>% 
+    select("Country","Region","Producer","Variety","Processing","Altitude","TastingNotes","DirectTrade") 
   
   #removes any that we didn't run
   coffee$URL <- i
@@ -141,8 +143,8 @@ for(i in URLs){
 
 #get names of coffees
 coffee_names <- coffee_names %>% 
-  separate(Name, into = c("Roaster","CoffeeName"), sep = case_when("Name" %like% "The Library Specialty Coffee " ~ "The Library Specialty Coffee ",
-                                                                   TRUE ~ "The Library "), remove = TRUE)
+  separate(Name, into = c("Roaster","CoffeeName"), sep = case_when("Name" %like% "Pilot Coffee Roasters " ~ "Pilot Coffee Roasters ",
+                                                                   TRUE ~ "Pilot "), remove = TRUE)
 #merge which coffees had data so we can join on index
 filtered_URLs <- merge(hypenated_data,URLs, by.x='URL', by.y ='y', all=FALSE)
 
@@ -165,5 +167,5 @@ final <- merge(coffee_names, coffee_table, by='URL', all=TRUE) %>% select(-URL)
 
 #write to csv
 path_out = here::here('R/outputs/')
-file_name = paste0(path_out, 'EightOunce_TLS_',rundate,".csv")
+file_name = paste0(path_out, 'EightOunce_Pilot_',rundate,".csv")
 write_csv(final,file_name)
